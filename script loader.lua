29f6ed8959cc759d54ce47cc3a172964e48d36e4
AigonV <88944725+AigonV@users.noreply.github.com>
2021-08-28 22:51:16 +0200

    Update script loader.lua

--- script loader.lua
+++ script loader.lua
@@ -1,7 +1,7 @@
 local key = _G.Key or _G.key
 local check = "https://raw.githubusercontent.com/AigonV/Relentless/master/checker.php?key=" .. key
 if game:HttpGet(check) == "Whitelisted" then
-loadstring(game:HttpGet("https://raw.githubusercontent.com/AigonV/Relentless/master/Obfuscated%20Linker.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/AigonV/Relentless/master/ObfuscatedLinker.lua"))()
 else
 loadstring(game:HttpGet("https://raw.githubusercontent.com/AigonV/Relentless/master/NoWLLoader.lua"))()
 end
