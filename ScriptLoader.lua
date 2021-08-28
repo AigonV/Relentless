be937c29d8049c0f2291ecff13ff262e7286ef9b
AigonV <88944725+AigonV@users.noreply.github.com>
2021-08-28 22:57:10 +0200

    Update ScriptLoader.lua

--- ScriptLoader.lua
+++ ScriptLoader.lua
@@ -3,5 +3,150 @@ local check = "https://raw.githubusercontent.com/AigonV/Relentless/master/checke
 if game:HttpGet(check) == "Whitelisted" then
 loadstring(game:HttpGet("https://raw.githubusercontent.com/AigonV/Relentless/master/ObfuscatedLinker.lua"))()
 else
-loadstring(game:HttpGet("https://raw.githubusercontent.com/AigonV/Relentless/master/NoWLLoader.lua"))()
+-- Gui to Lua
+-- Version: 3.2
+
+-- Instances:
+
+local CryoHubNWL = Instance.new("ScreenGui")
+local Frame = Instance.new("Frame")
+local Circle = Instance.new("ImageLabel")
+local text = Instance.new("TextLabel")
+local TextLabel = Instance.new("TextLabel")
+
+--Properties:
+
+CryoHubNWL.Name = "CryoHubNWL"
+CryoHubNWL.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
+CryoHubNWL.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
+CryoHubNWL.ResetOnSpawn = false
+
+Frame.Parent = CryoHubNWL
+Frame.AnchorPoint = Vector2.new(0.5, 0.5)
+Frame.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+Frame.BackgroundTransparency = 1.000
+Frame.ClipsDescendants = true
+Frame.Position = UDim2.new(0.5, 0, 0.5, 0)
+Frame.Size = UDim2.new(0, 384, 0, 220)
+
+Circle.Name = "Circle"
+Circle.Parent = Frame
+Circle.AnchorPoint = Vector2.new(0.5, 0.5)
+Circle.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+Circle.BackgroundTransparency = 1.000
+Circle.BorderSizePixel = 0
+Circle.ClipsDescendants = true
+Circle.Position = UDim2.new(0.5, 0, 0.5, 0)
+Circle.Size = UDim2.new(0, 500, 0, 500)
+Circle.Image = "rbxassetid://660373145"
+Circle.ImageColor3 = Color3.fromRGB(47, 47, 47)
+
+text.Name = "text"
+text.Parent = Circle
+text.AnchorPoint = Vector2.new(0.5, 0.5)
+text.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+text.BackgroundTransparency = 1.000
+text.Position = UDim2.new(0, 59, 0, 57)
+text.Size = UDim2.new(0, 119, 0, 50)
+text.Font = Enum.Font.SourceSans
+text.Text = "Welcome, 2vCF"
+text.TextColor3 = Color3.fromRGB(255, 255, 255)
+text.TextSize = 14.000
+text.TextTransparency = 1.000
+
+TextLabel.Parent = CryoHubNWL
+TextLabel.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel.Size = UDim2.new(0, 200, 0, 50)
+TextLabel.Visible = false
+TextLabel.Font = Enum.Font.SourceSans
+TextLabel.Text = ""
+TextLabel.TextColor3 = Color3.fromRGB(0, 0, 0)
+TextLabel.TextSize = 14.000
+
+-- Scripts:
+
+local function KCUBM_fake_script() -- Frame.Dragify 
+	local script = Instance.new('LocalScript', Frame)
+
+	local UIS = game:GetService("UserInputService")
+	function dragify(Frame)
+	    dragToggle = nil
+	    local dragSpeed = 0.25
+	    dragInput = nil
+	    dragStart = nil
+	    local dragPos = nil
+	    function updateInput(input)
+	        local Delta = input.Position - dragStart
+	        local Position = UDim2.new(startPos.X.Scale, startPos.X.Offset + Delta.X, startPos.Y.Scale, startPos.Y.Offset + Delta.Y)
+	        game:GetService("TweenService"):Create(Frame, TweenInfo.new(0.25), {Position = Position}):Play()
+	    end
+	    Frame.InputBegan:Connect(function(input)
+	        if (input.UserInputType == Enum.UserInputType.MouseButton1 or input.UserInputType == Enum.UserInputType.Touch) and UIS:GetFocusedTextBox() == nil then
+	            dragToggle = true
+	            dragStart = input.Position
+	            startPos = Frame.Position
+	            input.Changed:Connect(function()
+	                if input.UserInputState == Enum.UserInputState.End then
+	                    dragToggle = false
+	                end
+	            end)
+	        end
+	    end)
+	    Frame.InputChanged:Connect(function(input)
+	        if input.UserInputType == Enum.UserInputType.MouseMovement or input.UserInputType == Enum.UserInputType.Touch then
+	            dragInput = input
+	        end
+	    end)
+	    game:GetService("UserInputService").InputChanged:Connect(function(input)
+	        if input == dragInput and dragToggle then
+	            updateInput(input)
+	        end
+	    end)
+	end
+	
+	dragify(script.Parent)
+end
+coroutine.wrap(KCUBM_fake_script)()
+local function TCDU_fake_script() -- CryoHubNWL.intro 
+	local script = Instance.new('LocalScript', CryoHubNWL)
+
+	--made by aigon no skidding pls
+	local circle = script.Parent.Frame.Circle
+	local text = circle.text
+	local intron = UDim2.new(0, 119, 0, 115)
+	local on = UDim2.new(0, 500, 0, 500)
+	local off = UDim2.new(0, 0, 0, 0)
+	local uion = UDim2.new(0, 384, 0, 220)
+	local uion2 = UDim2.new(0, 384, 0, 21)
+	local uioff = UDim2.new(0, 0, 0, 21)
+	
+	circle.Size = off
+	text.Text = "Not whitelisted."
+	text.TextTransparency = 1
+	
+	wait(1)
+	
+	circle:TweenSize(intron, "InOut", "Sine", 0.4, true)
+	wait(0.5)
+	for i, v in pairs(circle:GetChildren()) do
+		if v:IsA("TextLabel") then
+			for i=1,10 do
+				text.TextTransparency = 1-(i/10)
+				wait()
+			end
+			wait(2)
+			game.Players.LocalPlayer:Kick("Not whitelisted.")
+		end
+	end
+end
+coroutine.wrap(TCDU_fake_script)()
+local function LWXMI_fake_script() -- TextLabel.LocalScript 
+	local script = Instance.new('LocalScript', TextLabel)
+
+	while wait() do
+		script.Parent.Text = _G.key or _G.Key
+	end
+end
+coroutine.wrap(LWXMI_fake_script)()
+
 end
